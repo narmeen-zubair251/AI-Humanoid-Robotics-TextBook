6c474cffa7092449dbca6908a4bcee676435f01c
narmeen <narmeenzubair021@gmail.com>
2025-12-17 19:20:35 +0500

    first commit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,12 +18,12 @@ const config: Config = {
   url: 'https://narmeen-zubair251.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ai-human-robotics-textbook/',
+  baseUrl: '/ai-humanoid-robotics-textbook/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'narmeen-zubair251', // Usually your GitHub org/user name.
-  projectName: 'AI-Human-Robotics-TextBook', // Usually your repo name.
+  projectName: 'AI-Humanoid-Robotics-TextBook', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
